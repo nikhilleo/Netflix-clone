2b787db7c5c372c4088c6127a8508eddf9bf1082
nikhilleo <ramrakhyani94@gmail.com>
2021-02-21 17:23:49 +0530

    Homescreen Added

--- netflix-clone/src/HomeScreen.js
+++ netflix-clone/src/HomeScreen.js
@@ -2,12 +2,54 @@ import React from 'react'
 import Banner from './Banner'
 import "./HomeScreen.css"
 import Nav from './Nav'
+import Rows from "./Rows"
+import requests from "./Request";
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
             <Nav/>
             <Banner/> 
+            <Rows
+                title="Netflix Originals"
+                fetchURL={requests.fetchNetflixOriginals}
+                isLarge={true}
+            />
+            <Rows
+                title="Top Rated"
+                fetchURL={requests.fetchTopRated}
+                // isLarge={true}
+            />
+            <Rows
+                title="Trending"
+                fetchURL={requests.fetchTrending}
+                // isLarge={true}
+            />
+            <Rows
+                title="Comedy"
+                fetchURL={requests.fetchComedy}
+                // isLarge={true}
+            />
+            <Rows
+                title="Action"
+                fetchURL={requests.fetchActionMovies}
+                // isLarge={true}
+            />
+            <Rows
+                title="Horror"
+                fetchURL={requests.fetchHorror}
+                // isLarge={true}
+            />
+            <Rows
+                title="Romance"
+                fetchURL={requests.fetchRomance}
+                // isLarge={true}
+            />
+            <Rows
+                title="Documentaries"
+                fetchURL={requests.fetchDocs}
+                // isLarge={true}
+            />
         </div>
     )
 }
