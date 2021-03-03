86da8b7fe1ccab1828dfc711bb4ccecbd29d47bf
nikhilleo <ramrakhyani94@gmail.com>
2021-03-03 22:35:46 +0530

    Routes For Netflix Originals

---
+++ netflix-server/routes/originals.js
@@ -0,0 +1,12 @@
+
+
+const express = require('express');
+const router = express.Router();
+const original_controllers = require("../controllers/originals_controller")
+
+
+router.get('/getOriginals',original_controllers.getOriginals);
+
+router.get('/updateOriginals',original_controllers.updateOriginals);
+
+module.exports = router;
--- netflix-server/routes/trending.js
+++ netflix-server/routes/trending.js
@@ -7,5 +7,6 @@ const trending_controllers = require("../controllers/trending_controllers")
 
 router.get('/getTrending',trending_controllers.getTrending);
 
+router.get('/updateTrending',trending_controllers.updateTrending);
 
 module.exports = router;
