66d5d0bef7e7f5e80d60b9a881b82134f3533da4
nikhilleo <ramrakhyani94@gmail.com>
2021-03-05 18:56:19 +0530

    Changes In Netflix Controller

--- netflix-server/controllers/originals_controller.js
+++ netflix-server/controllers/originals_controller.js
@@ -26,15 +26,23 @@ exports.updateOriginals = async(req,res)=>{
     await Originals.deleteMany({});
     const originals = await Axios.get(`${process.env.TMDB_BASE_URL}${process.env.fetchNetflixOriginals}`)
     netflix_originals = originals.data.results;
+    // console.log(netflix_originals);
     for (let i = 0; i < netflix_originals.length; i++) {
         if (netflix_originals[i].title || netflix_originals[i].name) {
+            // console.log(netflix_originals[i].overview);
+            // console.log(netflix_originals[i].release_date || netflix_originals[i].first_air_date);
             const data = {
                 tmdb_id: netflix_originals[i].id,
                 name: netflix_originals[i].title || netflix_originals[i].name,
+                title: netflix_originals[i].original_name,
                 overview: netflix_originals[i].overview,
                 date_of_release: netflix_originals[i].release_date || netflix_originals[i].first_air_date,
                 language: netflix_originals[i].original_language,
                 avg_votes: netflix_originals[i].vote_average,
+            }
+            if(!netflix_originals[i].first_air_date)
+            {
+                data.date_of_release = "Comimg Soon"
             }
             if (netflix_originals[i].title) {
                 var options = {
@@ -58,16 +66,21 @@ exports.updateOriginals = async(req,res)=>{
                         data.poster_img = img_url.url
                         var cnt = 1;
                         if (cnt == 1) {
-                            const r = await yts(`${data.name} English Dubbed Final Trailer ${data.date_of_release.split("-")[0]}`)
-                            const trailer_url = getURL(r.all, data.name)
-                            data.trailer_url = trailer_url
+                            const r = await yts(`${data.name} Netflix English Trailer ${data.date_of_release.split("-")[0]}`)
+                            const trailer_url1 = getURL(r.all, data.name,data.title)
+                            // console.log("\n\n\n\n\n")
+                            // console.log(trailer_url1).
+                            console.log(trailer_url1)
+                            data.trailer_url = trailer_url1;
+                            // data.trailer_url = trailer_url1
                             cnt++;
                         }
                         const originals = await new Originals(data);
                         await originals.save();
                         carousal_downUp(originals._id, originals.name, process.env.BACK_IMAGE_URL + netflix_originals[i].backdrop_path);
                         fs.unlink(filename, (err) => {
-                            if (err) console.log(err)
+                            if (err) 
+                            console.log(err)
                             else
                             console.log(`Deleted file: ${filename}`); 
                         });
@@ -114,7 +127,7 @@ function carousal_downUp(id, name, url) {
 }
 
 
-function getURL(data, name1) {
+function getURL(data, name1,title1) {
     var flag = 0;
     var goodLinks = []
     const name = name1.toLowerCase()
@@ -127,19 +140,32 @@ function getURL(data, name1) {
             goodLinks.push(data[i].url);
         }
     }
-    if (goodLinks.length < 1) {
-        for (let i = 0; i < data.length; i++) {
-            if (title.includes(":")) {
-                title.replace(":", "");
-            }
-            if (title.includes(`${name}`) && title.includes("official") && data[i].seconds < 300) {
-                goodLinks.push(data[i].url);
-            }
-        }
+    // console.log(goodLinks)
+    if (goodLinks.length <= 1) {
+        yts(`${title1} English Dubbed Final Trailer`).then((res)=>{
+            res.all.length = 10;
+            var data1 = res.all;
+            const data = newdata(data1.length=5,title1);
+            data.length = 10;
+            goodLinks = data
+        }).catch((err)=>{
+            console.log(err)
+        })
     }
     return goodLinks;
 }
 
+function newdata(data,title1)
+{
+    var goodLinks = []
+    for (let i = 0; i < data.length; i++) {
+        console.log(data);
+        // console.log(data[i].url)
+        goodLinks.push(data[i].url);
+    }
+    // console.log(goodLinks);
+    return goodLinks;
+}
 
 
 exports.getOriginals = async(req,res)=>{
