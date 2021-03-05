5a00a133c2075b9bd21631fdd59a5ad211a68a3a
nikhilleo <ramrakhyani94@gmail.com>
2021-03-05 18:51:55 +0530

    Banner Updated

--- netflix-clone/src/Banner/Banner.js
+++ netflix-clone/src/Banner/Banner.js
@@ -30,15 +30,15 @@ function Banner() {
             // })
             const movie = await Axios.get(requests.fetchNetflixOriginals);
             console.log(movie)
-            console.log(movie.data.results);
-            const no = Math.floor(Math.random() * movie.data.results.length  - 1);
+            console.log(movie.data);
+            const no = Math.floor(Math.random() * movie.data.length  - 1);
             console.log(no);
             if(no>=0){
-                setMovie(movie.data.results[no]);
+                setMovie(movie.data[no]);
             }
             else if(no<0)
             {
-                setMovie(movie.data.results[19])
+                setMovie(movie.data[19])
             }
             return movie;
         }
@@ -58,20 +58,20 @@ function Banner() {
     return (
 
         <div className="back" style={{
-            backgroundImage:`url("https://image.tmdb.org/t/p/original/${Movie?.backdrop_path}")`,
+            backgroundImage:`url(${Movie?.carousal_img})`,
             backgroundSize:"cover",
             backgroundPosition:"revert"}}>
             <div style={{backgroundImage:`linear-gradient(to right,#171717 0,rgba(23,23,23,.98) 20%,rgba(23,23,23,.97) 25%,rgba(23,23,23,.95) 35%,rgba(23,23,23,.94) 40%,rgba(23,23,23,.92) 45%,rgba(23,23,23,.9) 50%,rgba(23,23,23,.87) 55%,rgba(23,23,23,.82) 60%,rgba(23,23,23,.75) 65%,rgba(23,23,23,.63) 70%,rgba(23,23,23,.45) 75%,rgba(23,23,23,.27) 80%,rgba(23,23,23,.15) 85%,rgba(23,23,23,.08) 90%,rgba(23,23,23,.03) 95%,rgba(23,23,23,0) 100%)`, width:"70%"}}>
                 <div className="banner__contents">
-                    <h1 className="banner__title">{Movie?.title || Movie?.name || Movie?.original_name}</h1>
+                    <h1 className="banner__title">{Movie?.name}</h1>
                     <div className="banner__buttons">
                         <button className="banner__button" onClick={handleChange}>Play !!</button>
                         <button className="banner__button">My List</button>
                     </div>
                     <div className="extra_info">
-                        <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.first_air_date.split("-")[0] +  "  |  " + " "}</h4>
-                        <h4 className="first_air" style={{color:"#a3a3a3",textTransform:"uppercase"}} >{Movie?.original_language +  "  |"}</h4>
-                        <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.vote_average +  " / 10 "}</h4>
+                        <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.date_of_release.split("-")[0] +  "  |  " + " "}</h4>
+                        <h4 className="first_air" style={{color:"#a3a3a3",textTransform:"uppercase"}} >{Movie?.language +  "  |"}</h4>
+                        <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.avg_votes +  " / 10 "}</h4>
     
                 </div>
                 <div className="banner__description">
