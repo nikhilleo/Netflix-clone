8044ff0a5aac904cce12288935516f025d426db7
nikhilleo <ramrakhyani94@gmail.com>
2021-03-09 13:43:56 +0530

    Banner Undefined Handeled

--- netflix-clone/src/Banner/Banner.js
+++ netflix-clone/src/Banner/Banner.js
@@ -38,7 +38,8 @@ function Banner() {
             }
             else if(no<0)
             {
-                setMovie(movie.data[movie.data.length - 1])
+                console.log(movie.data.length);
+                setMovie(movie.data[10])
             }
             return movie;
         }
@@ -56,33 +57,40 @@ function Banner() {
     }
 
     return (
-
-        <div className="back" style={{
-            backgroundImage:`url(${Movie?.carousal_img ? Movie?.carousal_img : Movie?.poster_img})`,
-            backgroundSize:"cover",
-            backgroundPosition:"revert"}}>
-            <div style={{backgroundImage:`linear-gradient(to right,#171717 0,rgba(23,23,23,.98) 20%,rgba(23,23,23,.97) 25%,rgba(23,23,23,.95) 35%,rgba(23,23,23,.94) 40%,rgba(23,23,23,.92) 45%,rgba(23,23,23,.9) 50%,rgba(23,23,23,.87) 55%,rgba(23,23,23,.82) 60%,rgba(23,23,23,.75) 65%,rgba(23,23,23,.63) 70%,rgba(23,23,23,.45) 75%,rgba(23,23,23,.27) 80%,rgba(23,23,23,.15) 85%,rgba(23,23,23,.08) 90%,rgba(23,23,23,.03) 95%,rgba(23,23,23,0) 100%)`, width:"70%"}}>
-                <div className="banner__contents">
-                    <h1 className="banner__title">{Movie?.name}</h1>
-                    <div className="banner__buttons">
-                        <button className="banner__button" onClick={handleChange}>Play !!</button>
-                        <button className="banner__button">My List</button>
+        <>
+        {Movie ? (
+            <div className="back" style={{
+                backgroundImage:`url(${Movie?.carousal_img ? Movie?.carousal_img : Movie?.poster_img})`,
+                backgroundSize:"cover",
+                backgroundPosition:"revert"}}>
+                <div style={{backgroundImage:`linear-gradient(to right,#171717 0,rgba(23,23,23,.98) 20%,rgba(23,23,23,.97) 25%,rgba(23,23,23,.95) 35%,rgba(23,23,23,.94) 40%,rgba(23,23,23,.92) 45%,rgba(23,23,23,.9) 50%,rgba(23,23,23,.87) 55%,rgba(23,23,23,.82) 60%,rgba(23,23,23,.75) 65%,rgba(23,23,23,.63) 70%,rgba(23,23,23,.45) 75%,rgba(23,23,23,.27) 80%,rgba(23,23,23,.15) 85%,rgba(23,23,23,.08) 90%,rgba(23,23,23,.03) 95%,rgba(23,23,23,0) 100%)`, width:"70%"}}>
+                    <div className="banner__contents">
+                        <h1 className="banner__title">{Movie?.name}</h1>
+                        <div className="banner__buttons">
+                            <button className="banner__button" onClick={handleChange}>Play !!</button>
+                            <button className="banner__button">My List</button>
+                        </div>
+                        <div className="extra_info">
+                            <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.date_of_release.split("-")[0] +  "  |  " + " "}</h4>
+                            <h4 className="first_air" style={{color:"#a3a3a3",textTransform:"uppercase"}} >{Movie?.language +  "  |"}</h4>
+                            <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.avg_votes +  " / 10 "}</h4>
+        
                     </div>
-                    <div className="extra_info">
-                        <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.date_of_release.split("-")[0] +  "  |  " + " "}</h4>
-                        <h4 className="first_air" style={{color:"#a3a3a3",textTransform:"uppercase"}} >{Movie?.language +  "  |"}</h4>
-                        <h4 className="first_air" style={{color:"#a3a3a3"}} >{Movie?.avg_votes +  " / 10 "}</h4>
+                    <div className="banner__description">
+                        {truncate(`${Movie?.overview}`,200)} 
+                    </div>
+                    </div>  
+                </div>
+                
     
-                </div>
-                <div className="banner__description">
-                    {truncate(`${Movie?.overview}`,200)} 
-                </div>
-                </div>  
             </div>
-            
+           
+        ) : (
+            <div className="blank">
 
-        </div>
-
+            </div>
+        )}
+        </>
         // <header className="banner" style={{
         //     backgroundImage:`url("https://image.tmdb.org/t/p/original/${Movie?.backdrop_path}")`,
         //     backgroundSize:"cover",
