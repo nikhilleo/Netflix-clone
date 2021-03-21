a9b9229fa0208507529acbfec71bf811fa892cf8
nikhilleo <ramrakhyani94@gmail.com>
2021-03-21 15:29:38 +0530

    Added Router

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SignUp from "./SignUp/SignUp"
 import './App.css';
 import HomeScreen from "../src/HomeScreen/HomeScreen";
 
@@ -6,7 +13,16 @@ import HomeScreen from "../src/HomeScreen/HomeScreen";
 function App() {
   return (
     <div className="app">
-      <HomeScreen/>
+      <Router>
+        <Switch>
+          <Route exact path="/"> 
+            <HomeScreen/>
+          </Route>
+          <Route exact path="/signup"> 
+            <SignUp/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
