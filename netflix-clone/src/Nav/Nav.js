2fdc1bb2db48f7ba03ab3a9582a4619e9ed1d52c
nikhilleo <ramrakhyani94@gmail.com>
2021-03-21 22:30:14 +0530

    Navbar Changes

--- netflix-clone/src/Nav/Nav.js
+++ netflix-clone/src/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import logo from "../logo.png"
-import avatar from '../avatar_logo.png'
 import "./Nav.css";
+import { Link } from 'react-router-dom';
 
 function Nav() {
 
@@ -26,12 +26,14 @@ function Nav() {
     return (
         <div className={`nav ${show ? "nav__black" : ""}`}>
             <div className="nav__content">
+            <Link to="/">
                 <div>
                     <img className="nav__logo" src={logo} alt=""/>
                 </div>
+            </Link>
                 <div className="nav_btns" >
                 {/* <img className="nav__avatar" src={avatar} alt=""/> */}
-                <button className="join_now" type="submit">Join Now </button>
+                <Link to="/signup"><button className="join_now" type="submit">Join Now </button></Link>
                 <button className="join_now1" type="submit">Sign In </button>
                 </div>
             </div>
