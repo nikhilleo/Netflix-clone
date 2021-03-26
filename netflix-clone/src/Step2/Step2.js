2e36026d948739aff5dc34bad77d67764966f01e
nikhilleo <ramrakhyani94@gmail.com>
2021-03-27 01:11:26 +0530

    Step2 For Desktop

--- netflix-clone/src/Step2/Step2.js
+++ netflix-clone/src/Step2/Step2.js
@@ -74,20 +74,57 @@ function Step2() {
                     <h4>Change or cancel your plan anytime.</h4>
                 </div>
             </div>
-           <div className="plan_main">
-                <div onClick={handleClick} className="plan_div">
-                    Mobile
+            <div className="plans_div">
+                <div className="plan_main">
+                    <div onClick={handleClick} className="plan_div">
+                        Mobile
+                    </div>
+                    <div  onClick={handleClick} className="plan_div">
+                    Basic
+                    </div>
+                    <div  onClick={handleClick} className="plan_div">
+                        Standard
+                    </div>
+                    <div  onClick={handleClick} className="plan_div">
+                        Premium
+                    </div>
                 </div>
-                <div  onClick={handleClick} className="plan_div">
-                   Basic
-                </div>
-                <div  onClick={handleClick} className="plan_div">
-                    Standard
-                </div>
-                <div  onClick={handleClick} className="plan_div">
-                    Premium
-                </div>
-           </div>
+                <table className="ab" >
+                    <tr style={{fontSize:'17px'}} >
+                        <td style={{width:"40vw"}}>Monthly Plan</td>
+                        <td style={{width:"16vw"}}>₹ 199</td>
+                        <td style={{width:"15vw"}}>₹ 499</td>
+                        <td style={{width:"14vw"}}>₹ 649</td>
+                        <td style={{width:"11vw"}}>₹ 799</td>
+                    </tr>
+                </table>
+                <table className="ab" >
+                    <tr style={{fontSize:'17px'}}>
+                        <td style={{width:"40vw"}}>Video quality</td>
+                        <td style={{width:"15vw"}}>Good</td>
+                        <td style={{width:"15vw"}}>Good</td>
+                        <td style={{width:"14vw"}}>Better</td>
+                        <td style={{width:"9vw"}}>Best</td>
+                    </tr>
+                </table>
+                <table className="ab" >
+                    <tr style={{fontSize:'17px'}}>
+                        <td style={{width:"40vw"}}>Resolution</td>
+                        <td style={{width:"15vw"}}>480p</td>
+                        <td style={{width:"15vw"}}>480p</td>
+                        <td style={{width:"14vw"}}>1080p</td>
+                        <td style={{width:"9vw"}}>4K+HDR</td>
+                    </tr>
+                </table>
+            <div className="extra__step2">
+                <p>
+                Full HD (1080p), Ultra HD (4K) and HDR availability subject to your internet service and device capabilities. Not all content available in HD, Full HD, Ultra HD or HDR. See Terms of Use for more details.
+                </p>
+                <p>Only people who live with you may use your account. Watch on 4 different devices at the same time with Premium, 2 with Standard, and 1 with Basic and Mobile.</p>
+                <Link to="/step3"><button className="to_step_2" type="submit">Next </button></Link>
+            </div>
+            </div>
+            
         </div>
     )
 }
