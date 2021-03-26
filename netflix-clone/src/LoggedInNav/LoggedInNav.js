53c81bfd9770d2aa196932dee69b3440feaaad16
nikhilleo <ramrakhyani94@gmail.com>
2021-03-26 21:21:56 +0530

    Navbar For Select Plan

---
+++ netflix-clone/src/LoggedInNav/LoggedInNav.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./LoggedInNav.css"
+import logo from "../logo.png"
+
+
+function LoggedInNav() {
+    return (
+        <div className="nav_loggedIn">
+             <Link to="/signup">
+                <div>
+                    <img className="nav__logo1" src={logo} alt=""/>
+                </div>
+            </Link>  
+            <Link to="/signin"><button className="join_nowLogged" type="submit">Sign Out </button></Link>
+                  
+        </div>
+    )
+}
+
+export default LoggedInNav
